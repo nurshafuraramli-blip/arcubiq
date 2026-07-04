c30fcf25d0ee4f8874772ba7f0d11b65ae4df950
Nur Shafura Ramli <nur.shafura.ramli@gmail.com>
2026-07-04 17:45:35 +0800

    Tambah kotak penerangan

--- script.js
+++ script.js
@@ -1,15 +1,25 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded",()=>{
 
-  const marker = document.querySelector('[mindar-image-target]');
-  const audio = document.querySelector("#heartbeat");
+const marker=document.querySelector("[mindar-image-target]");
+const audio=document.querySelector("#heartbeat");
+const info=document.querySelector("#infoBox");
 
-  marker.addEventListener("targetFound", () => {
-      audio.play();
-  });
+marker.addEventListener("targetFound",()=>{
 
-  marker.addEventListener("targetLost", () => {
-      audio.pause();
-      audio.currentTime = 0;
-  });
+audio.play();
+
+info.style.display="block";
+
+});
+
+marker.addEventListener("targetLost",()=>{
+
+audio.pause();
+
+audio.currentTime=0;
+
+info.style.display="none";
+
+});
 
 });
